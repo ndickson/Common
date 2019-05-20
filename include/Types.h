5af82234fff0fe52c856ba6bc0eae5c57ce3b5b6
ndickson <3254194+ndickson@users.noreply.github.com>
2019-05-20 00:35:48 -0400

    Added more types

--- include/Types.h
+++ include/Types.h
@@ -24,8 +24,7 @@ using uint32 = uint32_t;
 using uint64 = uint64_t;
 
 // Forward declarations
-template<typename SUBCLASS,typename T,size_t N>
-struct BaseVec;
+
 template<typename T,size_t N>
 struct Vec;
 
@@ -42,6 +41,33 @@ using Vec3f = Vec3<float>;
 using Vec3d = Vec3<double>;
 using Vec3i = Vec3<int32>;
 using Vec3I = Vec3<int64>;
+
+
+template<typename T>
+struct Span;
+
+using Spanf = Span<float>;
+using Spand = Span<double>;
+using Spani = Span<int32>;
+using SpanI = Span<int64>;
+
+
+template<typename T,size_t N>
+struct Box;
+
+template<typename T>
+using Box2 = Box<T,2>;
+template<typename T>
+using Box3 = Box<T,3>;
+
+using Box2f = Box2<float>;
+using Box2d = Box2<double>;
+using Box2i = Box2<int32>;
+using Box2I = Box2<int64>;
+using Box3f = Box3<float>;
+using Box3d = Box3<double>;
+using Box3i = Box3<int32>;
+using Box3I = Box3<int64>;
 
 LIBRARY_NAMESPACE_END
 OUTER_NAMESPACE_END
