3f040e6f34d1c8fbf4684100a0a81c0d8505fdd1
ndickson <3254194+ndickson@users.noreply.github.com>
2019-09-14 00:00:52 -0400

    Added backslashEscapedStringLength; updated unescapeBackslash

--- include/text/EscapeText.h
+++ include/text/EscapeText.h
@@ -46,8 +46,14 @@ void escapeBackslash(const char* inBegin, const char*const inEnd, Array<char>& o
 // If stopToken is not nullptr, it must point to a null-terminated UTF-8 text string,
 // indicating when to stop early, e.g. a double-quote, making parsing a quoted string
 // inside a larger string easier.  The start of the match will be considered the
-// end of the input string.  Note that stopToken will not be checked-for inside of
-// escape sequences.
+// end of the input string.  Note that stopToken will not be checked-for partway
+// through escape sequences.
+//
+// The return value is the address in the input string where unescaping stopped,
+// either because text matching stopToken was reached, or inEnd is nullptr and
+// a zero byte was reached, or inEnd is not nullptr and inEnd was reached.
+// For example, if stopToken contains just a double-quote, this would be the
+// end of the quoted string.
 //
 // In all, this function unescapes:
 //     \\ as backslash
@@ -68,7 +74,24 @@ void escapeBackslash(const char* inBegin, const char*const inEnd, Array<char>& o
 //     \u#### where # are all hexadecimal as hexadecimal UTF code point
 //     \U######## where # are all hexadecimal as hexadecimal UTF code point
 // Any other escape sequences are copied as is without the backslash.
-void unescapeBackslash(const char* inBegin, const char*const inEnd, Array<char>& outText, const char*const stopToken = nullptr, const BackslashEscapeStyle style = BackslashEscapeStyle::C);
+// Hexadecimal escape sequences with too few digits will be accepted
+// as long as there is at least one hexadecimal digit.
+const char* unescapeBackslash(const char* inBegin, const char*const inEnd, Array<char>& outText, const char*const stopToken = nullptr, const BackslashEscapeStyle style = BackslashEscapeStyle::C);
+
+// This function finds the length in bytes of the text string starting at inBegin,
+// and ending with either text matching stopToken, or a zero byte if inEnd is nullptr,
+// or inEnd if inEnd is not nullptr, taking into account that stopToken cannot
+// be matched starting partway through an escape sequence.  This is
+// equivalent to the return value of unescapeBackslash minus inBegin, so can
+// be used for finding the ends of strings containing escape sequences.
+//
+// If unescapedLength is not nullptr, the length of the unescaped equivalent of the
+// text string will be computed and written there.  This is equivalent to the
+// number of bytes that would be appended to outText in unescapeBackslash,
+// so can be used to determine the size of buffer to preallocate, if needed.
+//
+// See unescapeBackslash for specifics on how the escape sequences are interpreted
+size_t backslashEscapedStringLength(const char* inBegin, const char*const inEnd, const char*const stopToken = nullptr, size_t* unescapedLength = nullptr, const BackslashEscapeStyle style = BackslashEscapeStyle::C);
 
 } // namespace text
 OUTER_NAMESPACE_END
