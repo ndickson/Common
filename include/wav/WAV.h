e46ec8c796eb1378e5a7636a4f3bec2419cf3187
ndickson <3254194+ndickson@users.noreply.github.com>
2019-11-08 00:52:09 -0500

    Marked functions in WAV.h as COMMON_LIBRARY_EXPORTED

--- include/wav/WAV.h
+++ include/wav/WAV.h
@@ -22,10 +22,10 @@ struct AudioTracks {
 template<typename T>
 bool ReadWAVFile(const char* filename, AudioTracks<T>& tracks);
 
-extern template bool ReadWAVFile<uint8>(const char* filename, AudioTracks<uint8>& tracks);
-extern template bool ReadWAVFile<int16>(const char* filename, AudioTracks<int16>& tracks);
-extern template bool ReadWAVFile<float>(const char* filename, AudioTracks<float>& tracks);
-extern template bool ReadWAVFile<double>(const char* filename, AudioTracks<double>& tracks);
+extern template COMMON_LIBRARY_EXPORTED bool ReadWAVFile<uint8>(const char* filename, AudioTracks<uint8>& tracks);
+extern template COMMON_LIBRARY_EXPORTED bool ReadWAVFile<int16>(const char* filename, AudioTracks<int16>& tracks);
+extern template COMMON_LIBRARY_EXPORTED bool ReadWAVFile<float>(const char* filename, AudioTracks<float>& tracks);
+extern template COMMON_LIBRARY_EXPORTED bool ReadWAVFile<double>(const char* filename, AudioTracks<double>& tracks);
 
 // Instantiated for uint8, int16, float, and double.
 // outputBitsPerSample 8 or less will use uint8, up to 16 will use int16,
@@ -33,10 +33,10 @@ extern template bool ReadWAVFile<double>(const char* filename, AudioTracks<doubl
 template<typename T>
 bool WriteWAVFile(const char* filename, const AudioTracks<T>& tracks, size_t outputBitsPerSample);
 
-extern template bool WriteWAVFile<uint8>(const char* filename, const AudioTracks<uint8>& tracks, size_t outputBitsPerSample);
-extern template bool WriteWAVFile<int16>(const char* filename, const AudioTracks<int16>& tracks, size_t outputBitsPerSample);
-extern template bool WriteWAVFile<float>(const char* filename, const AudioTracks<float>& tracks, size_t outputBitsPerSample);
-extern template bool WriteWAVFile<double>(const char* filename, const AudioTracks<double>& tracks, size_t outputBitsPerSample);
+extern template COMMON_LIBRARY_EXPORTED bool WriteWAVFile<uint8>(const char* filename, const AudioTracks<uint8>& tracks, size_t outputBitsPerSample);
+extern template COMMON_LIBRARY_EXPORTED bool WriteWAVFile<int16>(const char* filename, const AudioTracks<int16>& tracks, size_t outputBitsPerSample);
+extern template COMMON_LIBRARY_EXPORTED bool WriteWAVFile<float>(const char* filename, const AudioTracks<float>& tracks, size_t outputBitsPerSample);
+extern template COMMON_LIBRARY_EXPORTED bool WriteWAVFile<double>(const char* filename, const AudioTracks<double>& tracks, size_t outputBitsPerSample);
 
 } // namespace wav
 OUTER_NAMESPACE_END
