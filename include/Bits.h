842d2df4d3a4872cb20bb073ff9388f014f7abad
ndickson <3254194+ndickson@users.noreply.github.com>
2020-04-08 01:02:21 -0400

    Added uint16 and uint8 overloads of bitScanF32 and bitScanR32

--- include/Bits.h
+++ include/Bits.h
@@ -87,6 +87,41 @@ COMMON_LIBRARY_NAMESPACE_BEGIN
 	return count;
 }
 
+[[nodiscard]] constexpr inline uint32 bitScanF32(uint16 v) {
+	// NOTE: This doesn't use _BitScanForward (Visual Studio) or __builtin_ctz (GCC/Clang)
+	//       because _BitScanForward can't be called from constexpr code.
+	if (v == 0) {
+		return uint32(32);
+	}
+	bool has_8 = ((v&0xFF) != 0);
+	uint32 count = uint32(!has_8)<<3;
+	v = has_8 ? v : (v>>8);
+	bool has_4 = ((v&0xF) != 0);
+	count += uint32(!has_4)<<2;
+	v = has_4 ? v : (v>>4);
+	bool has_2 = ((v&0x3) != 0);
+	count += uint32(!has_2)<<1;
+	v = has_2 ? v : (v>>2);
+	count += !(v&1);
+	return count;
+}
+
+[[nodiscard]] constexpr inline uint32 bitScanF32(uint8 v) {
+	// NOTE: This doesn't use _BitScanForward (Visual Studio) or __builtin_ctz (GCC/Clang)
+	//       because _BitScanForward can't be called from constexpr code.
+	if (v == 0) {
+		return uint32(32);
+	}
+	bool has_4 = ((v&0xF) != 0);
+	uint32 count = uint32(!has_4)<<2;
+	v = has_4 ? v : (v>>4);
+	bool has_2 = ((v&0x3) != 0);
+	count += uint32(!has_2)<<1;
+	v = has_2 ? v : (v>>2);
+	count += !(v&1);
+	return count;
+}
+
 [[nodiscard]] constexpr inline uint32 bitScanR64(uint64 v) {
 	// NOTE: This doesn't use _BitScanReverse64 (Visual Studio) or __builtin_clzll (GCC/Clang)
 	//       because _BitScanReverse64 can't be called from constexpr code.
@@ -126,6 +161,41 @@ COMMON_LIBRARY_NAMESPACE_BEGIN
 	v = has_8 ? (v>>8) : v;
 	bool has_4 = ((v>>4) != 0);
 	count += uint32(has_4)<<2;
+	v = has_4 ? (v>>4) : v;
+	bool has_2 = ((v>>2) != 0);
+	count += uint32(has_2)<<1;
+	v = has_2 ? (v>>2) : v;
+	count += uint32(v>>1);
+	return count;
+}
+
+[[nodiscard]] constexpr inline uint32 bitScanR32(uint16 v) {
+	// NOTE: This doesn't use _BitScanReverse (Visual Studio) or __builtin_clz (GCC/Clang)
+	//       because _BitScanReverse can't be called from constexpr code.
+	if (v == 0) {
+		return uint32(-1);
+	}
+	bool has_8 = ((v>>8) != 0);
+	uint32 count = uint32(has_8)<<3;
+	v = has_8 ? (v>>8) : v;
+	bool has_4 = ((v>>4) != 0);
+	count += uint32(has_4)<<2;
+	v = has_4 ? (v>>4) : v;
+	bool has_2 = ((v>>2) != 0);
+	count += uint32(has_2)<<1;
+	v = has_2 ? (v>>2) : v;
+	count += uint32(v>>1);
+	return count;
+}
+
+[[nodiscard]] constexpr inline uint32 bitScanR32(uint8 v) {
+	// NOTE: This doesn't use _BitScanReverse (Visual Studio) or __builtin_clz (GCC/Clang)
+	//       because _BitScanReverse can't be called from constexpr code.
+	if (v == 0) {
+		return uint32(-1);
+	}
+	bool has_4 = ((v>>4) != 0);
+	uint32 count = uint32(has_4)<<2;
 	v = has_4 ? (v>>4) : v;
 	bool has_2 = ((v>>2) != 0);
 	count += uint32(has_2)<<1;
