9959e91078089636b096a8892cc9f06d47786a1b
ndickson <3254194+ndickson@users.noreply.github.com>
2020-04-18 23:33:51 -0400

    Implemented several more operators in math::parseExpression

--- src/math/Expression.cpp
+++ src/math/Expression.cpp
@@ -3,6 +3,7 @@
 // Declarations of the functions are in Expression.h
 
 #include "math/Expression.h"
+#include "text/EscapeText.h"
 #include "text/TextFunctions.h"
 #include "../Types.h"
 #include "../Array.h"
@@ -267,16 +268,9 @@ bool parseExpression(const char* begin, const char* end, Array<Item>& output) {
 			}
 			else if (expectingUnary) {
 				// Unary plus or minus
-				++text;
-				if (text == end || *text == 0) {
-					// Unexpected end
-					return false;
-				}
-
 				ItemType type = (c == '+') ? ItemType::PREFIX_PLUS : ItemType::PREFIX_MINUS;
-
 				// Increment and decrement are handled above.
-				assert(*text != c);
+				assert(c != c1);
 
 				// FIXME: Handle if this is at the start of a first function parameter!!!
 
@@ -288,19 +282,156 @@ bool parseExpression(const char* begin, const char* end, Array<Item>& output) {
 				appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
 			}
 		}
-		else if (c == '*' || c == '/' || c == '%') {
+		else if (c == '*') {
+			if (text == end || *text == 0) {
+				// Unexpected end
+				return false;
+			}
+			char c1 = *text;
+			if (expectingUnary) {
+				// Unary star
+				ItemType type = ItemType::PREFIX_STAR;
 
+				// FIXME: Handle if this is at the start of a first function parameter!!!
+
+				operatorStack.append(Item{type,1,tokenStart,text});
+			}
+			else if (c1 == '=') {
+				// Assign-multiply "*="
+				++text;
+				ItemType type = ItemType::ASSIGN_MULTIPLY;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, true, tokenStart, text);
+			}
+			else if (c1 == '*') {
+				// Power "**"
+				++text;
+				ItemType type = ItemType::POWER;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+			}
+			else {
+				// Regular multiply "*"
+				ItemType type = ItemType::MULTIPLY;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+			}
+		}
+		else if (c == '/') {
+			if (text == end || *text == 0) {
+				// Unexpected end
+				return false;
+			}
+			char c1 = *text;
+			if (c1 == '=') {
+				// Assign-divide "/="
+				++text;
+				ItemType type = ItemType::ASSIGN_DIVIDE;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, true, tokenStart, text);
+			}
+			else if (c1 == '/') {
+				// Comment "//"
+
+				// FIXME: Implement this!!!
+
+
+			}
+			else if (c1 == '*') {
+				// Comment "/*"
+
+				// FIXME: Implement this!!!
+
+			}
+			else {
+				// Regular divide "/"
+				ItemType type = ItemType::DIVIDE;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+			}
+		}
+		else if (c == '\\') {
+			if (text == end || *text == 0) {
+				// Unexpected end
+				return false;
+			}
+			char c1 = *text;
+			if (c1 == '\\') {
+				// Left divide "\\"
+				++text;
+				ItemType type = ItemType::LEFT_DIVIDE;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+			}
+			else {
+				// Line continuation
+
+				// FIXME: Implement this!!!
+			}
+		}
+		else if (c == '%') {
+			if (text == end || *text == 0) {
+				// Unexpected end
+				return false;
+			}
+			char c1 = *text;
+			if (c1 == '=') {
+				// Assign-modulus "%="
+				++text;
+				ItemType type = ItemType::ASSIGN_MODULUS;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, true, tokenStart, text);
+			}
+			else {
+				// Regular modulus "%"
+				ItemType type = ItemType::MODULUS;
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+			}
 		}
 		else if (c == '&' || c == '^' || c == '|') {
+			if (expectingUnary) {
+				if (c == '&') {
+					// Unary ampersand
+					ItemType type = ItemType::PREFIX_AMPERSAND;
 
+					// FIXME: Handle if this is at the start of a first function parameter!!!
+
+					operatorStack.append(Item{type,1,tokenStart,text});
+				}
+				else {
+					// "^", "|", "^=", "|=", and "||" can only be binary operators.
+					unexpectedBinary = true;
+				}
+			}
+			else {
+				if (text == end || *text == 0) {
+					// Unexpected end
+					return false;
+				}
+				char c1 = *text;
+				if (c1 == '=') {
+					// Assign-and "&=", assign-xor "^=", or assign-or "|="
+					++text;
+					ItemType type = (c == '&') ? ItemType::ASSIGN_AND
+						: ((c == '^') ? ItemType::ASSIGN_XOR : ItemType::ASSIGN_OR);
+					unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, true, tokenStart, text);
+				}
+				else if (c1 == c && c != '^') {
+					// Logical and "&&" or logical or "||"
+					++text;
+					ItemType type = (c == '&') ? ItemType::LOGICAL_AND : ItemType::LOGICAL_OR;
+					unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+				}
+				else {
+					// Bitwise and "&", bitwise xor "^", or bitwise or "|"
+					ItemType type = (c == '&') ? ItemType::BITWISE_AND
+						: ((c == '^') ? ItemType::BITWISE_XOR : ItemType::BITWISE_OR);
+					unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+				}
+			}
 		}
 		else if (c == '<' || c == '>') {
-
+			// FIXME: Implement this!!!
 		}
 		else if (c == '?') {
+			// FIXME: Implement this!!!
 
 		}
 		else if (c == ':') {
+			// FIXME: Implement this!!!
 
 		}
 		else if (c == '!') {
@@ -323,20 +454,62 @@ bool parseExpression(const char* begin, const char* end, Array<Item>& output) {
 			}
 			else {
 				// "!" can only be a unary operator.
+				// FIXME: Support implicit binary operator!!!
 				unexpectedUnary = true;
 			}
 		}
 		else if (c == '~') {
+			if (expectingUnary) {
+				// Bitwise not "~"
+				ItemType type = ItemType::PREFIX_BITWISE_NOT;
 
+				// FIXME: Handle if this is at the start of a first function parameter!!!
+
+				operatorStack.append(Item{type,1,tokenStart,text});
+			}
+			else {
+				// "~" can only be a unary operator.
+				// FIXME: Support implicit binary operator!!!
+				unexpectedUnary = true;
+			}
 		}
 		else if (c == '\"' || c == '\'') {
+			// Quoted string
+			const char stopToken[2] = {c, 0};
+			size_t stringEscapedLength = text::backslashEscapedStringLength(text, end, stopToken);
+			if (text + stringEscapedLength == end) {
+				// Unexpected end
+				return false;
+			}
+			text += stringEscapedLength+1;
 
+			if (expectingUnary) {
+				output.append(Item{ItemType::STRING_LITERAL, 0, tokenStart, text});
+			}
+			else {
+				// FIXME: Support implicit binary operator!!!
+				unexpectedUnary = true;
+			}
 		}
 		else if (c == '.') {
-
+			if (text == end || *text == 0) {
+				// Unexpected end
+				return false;
+			}
+			char c1 = *text;
+			if (c1 >= '0' && c1 <= '9') {
+				// Floating-point literal
+				// FIXME: Implement this!!!
+			}
+			else {
+				ItemType type = ItemType::DOT;
+				if (c1 == '*') {
+					++text;
+					type = ItemType::DOT_STAR;
+				}
+				unexpectedBinary = !appendBinaryOperator(type, output, operatorStack, expectingUnary, false, tokenStart, text);
+			}
 		}
-
-
 
 		if (unexpectedUnary) {
 			// Unexpected unary operator
@@ -349,7 +522,9 @@ bool parseExpression(const char* begin, const char* end, Array<Item>& output) {
 
 	}
 
+	// FIXME: Update operatorStack and output!!!
 
+	return true;
 }
 
 
